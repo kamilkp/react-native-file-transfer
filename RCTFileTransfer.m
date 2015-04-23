970365ebb3ae58ac445dc3376e2463c980de072e
Kamil Pękala <kamilkp@gmail.com>
2015-04-23 09:10:57 +0200

    v 0.0.1

---
+++ RCTFileTransfer.m
@@ -0,0 +1,145 @@
+//
+//  RCTFileTransfer.m
+//  react-native-file-transfer
+//
+//  Created by Kamil Pękala on 23.04.2015
+//  Copyright (c) 2015 Kamil Pękala. All rights reserved.
+//
+
+#import "RCTBridgeModule.h"
+#import <AssetsLibrary/AssetsLibrary.h>
+#import <UIKit/UIKit.h>
+@interface FileTransfer : NSObject <RCTBridgeModule>
+- (NSMutableURLRequest *)getMultiPartRequest:(NSData *)fileData serverUrl:(NSString *)server requestData:(NSDictionary *)requestData mimeType:(NSString *)mimeType fileName:(NSString *)fileName;
+- (void)uploadAssetsLibrary:(NSDictionary *)input callback:(RCTResponseSenderBlock)callback;
+- (void)uploadBase64:(NSDictionary *)input callback:(RCTResponseSenderBlock)callback;
+@end
+
+@implementation FileTransfer
+
+RCT_EXPORT_MODULE();
+
+RCT_EXPORT_METHOD(upload:(NSDictionary *)input callback:(RCTResponseSenderBlock)callback)
+{
+  NSString *url = input[@"path"];
+  if([url hasPrefix:@"assets-library"]){
+    [self uploadAssetsLibrary:input callback:callback];
+  }
+  else if([url hasPrefix:@"data:"]){
+    [self uploadBase64:input callback:callback];
+  }
+  else{
+    NSDictionary *res=[[NSDictionary alloc] initWithObjectsAndKeys:[NSNumber numberWithInteger:0],@"status",@"Unknown protocol",@"data",nil];
+    callback(@[res]);
+  }
+}
+
+- (void)uploadAssetsLibrary:(NSDictionary *)input callback:(RCTResponseSenderBlock)callback
+{
+
+  NSURL *url = [[NSURL alloc] initWithString:input[@"path"]];
+  NSString *fileName = input[@"fileName"];
+  NSString *mimeType = input[@"mimeType"];
+  NSString *uploadUrl = input[@"uploadUrl"];
+
+  ALAssetsLibrary *library = [[ALAssetsLibrary alloc] init];
+
+  // Using the ALAssetsLibrary instance and our NSURL object open the image.
+  [library assetForURL:url resultBlock:^(ALAsset *asset) {
+
+    ALAssetRepresentation *rep = [asset defaultRepresentation];
+    Byte *buffer = (Byte*)malloc(rep.size);
+    NSUInteger buffered = [rep getBytes:buffer fromOffset:0.0 length:rep.size error:nil];
+
+    NSData *fileData = [NSData dataWithBytesNoCopy:buffer length:buffered freeWhenDone:YES];
+    NSDictionary* requestData = [input objectForKey:@"data"];
+    NSMutableURLRequest* req = [self getMultiPartRequest:fileData serverUrl:uploadUrl requestData:requestData mimeType:mimeType fileName:fileName];
+
+    NSHTTPURLResponse *response = nil;
+    NSData *returnData = [NSURLConnection sendSynchronousRequest:req returningResponse:&response error:nil];
+    NSInteger statusCode = [response statusCode];
+    NSString *returnString = [[NSString alloc] initWithData:returnData encoding:NSUTF8StringEncoding];
+
+    NSDictionary *res=[[NSDictionary alloc] initWithObjectsAndKeys:[NSNumber numberWithInteger:statusCode],@"status",returnString,@"data",nil];
+
+    callback(@[res]);
+
+  } failureBlock:^(NSError *error) {
+    NSLog(@"Getting file from library failed: %@", error);
+  }];
+}
+
+- (void)uploadBase64:(NSDictionary *)input callback:(RCTResponseSenderBlock)callback
+{
+  NSString *fileName = input[@"fileName"];
+  NSString *mimeType = input[@"mimeType"];
+  NSString *uploadUrl = input[@"uploadUrl"];
+  NSString *dataUrlString = input[@"path"];
+  NSURL *dataUrl = [[NSURL alloc] initWithString:dataUrlString];
+
+  NSData *fileData = [NSData dataWithContentsOfURL: dataUrl];
+
+  NSDictionary* requestData = [input objectForKey:@"data"];
+  NSMutableURLRequest* req = [self getMultiPartRequest:fileData serverUrl:uploadUrl requestData:requestData mimeType:mimeType fileName:fileName];
+
+  NSHTTPURLResponse *response = nil;
+  NSData *returnData = [NSURLConnection sendSynchronousRequest:req returningResponse:&response error:nil];
+  NSInteger statusCode = [response statusCode];
+  NSString *returnString = [[NSString alloc] initWithData:returnData encoding:NSUTF8StringEncoding];
+
+  NSDictionary *res=[[NSDictionary alloc] initWithObjectsAndKeys:[NSNumber numberWithInteger:statusCode],@"status",returnString,@"data",nil];
+
+  callback(@[res]);
+}
+
+- (NSMutableURLRequest *)getMultiPartRequest:(NSData *)fileData serverUrl:(NSString *)server requestData:(NSDictionary *)requestData mimeType:(NSString *)mimeType fileName:(NSString *)fileName
+{
+  NSString* fileKey = @"file";
+  NSURL* url = [NSURL URLWithString:server];
+  NSMutableURLRequest* req = [NSMutableURLRequest requestWithURL:url];
+
+  [req setHTTPMethod:@"POST"];
+
+  NSString* formBoundaryString = @"----react.file.transfer.form.boundary";
+
+  NSString* contentType = [NSString stringWithFormat:@"multipart/form-data; boundary=%@", formBoundaryString];
+  [req setValue:contentType forHTTPHeaderField:@"Content-Type"];
+
+  NSData* formBoundaryData = [[NSString stringWithFormat:@"--%@\r\n", formBoundaryString] dataUsingEncoding:NSUTF8StringEncoding];
+  NSMutableData* requestBody = [NSMutableData data];
+
+  for (NSString* key in requestData) {
+    id val = [requestData objectForKey:key];
+    if ([val respondsToSelector:@selector(stringValue)]) {
+      val = [val stringValue];
+    }
+    if (![val isKindOfClass:[NSString class]]) {
+      continue;
+    }
+
+    [requestBody appendData:formBoundaryData];
+    [requestBody appendData:[[NSString stringWithFormat:@"Content-Disposition: form-data; name=\"%@\"\r\n\r\n", key] dataUsingEncoding:NSUTF8StringEncoding]];
+    [requestBody appendData:[val dataUsingEncoding:NSUTF8StringEncoding]];
+    [requestBody appendData:[@"\r\n" dataUsingEncoding : NSUTF8StringEncoding]];
+  }
+
+  [requestBody appendData:formBoundaryData];
+  [requestBody appendData:[[NSString stringWithFormat:@"Content-Disposition: form-data; name=\"%@\"; filename=\"%@\"\r\n", fileKey, fileName] dataUsingEncoding:NSUTF8StringEncoding]];
+  if (mimeType != nil) {
+    [requestBody appendData:[[NSString stringWithFormat:@"Content-Type: %@\r\n", mimeType] dataUsingEncoding:NSUTF8StringEncoding]];
+  }
+  [requestBody appendData:[[NSString stringWithFormat:@"Content-Length: %ld\r\n\r\n", (long)[fileData length]] dataUsingEncoding:NSUTF8StringEncoding]];
+
+  NSData* afterFile = [[NSString stringWithFormat:@"\r\n--%@--\r\n", formBoundaryString] dataUsingEncoding:NSUTF8StringEncoding];
+
+  long long totalPayloadLength = [requestBody length] + [fileData length] + [afterFile length];
+  [req setValue:[[NSNumber numberWithLongLong:totalPayloadLength] stringValue] forHTTPHeaderField:@"Content-Length"];
+
+  [requestBody appendData:fileData];
+  [requestBody appendData:afterFile];
+  [req setHTTPBody:requestBody];
+  return req;
+}
+
+@end
+
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # react-native-file-transfer
-react-native plugin for uploading files with multipart/form-data requests
+This little plugin lets you easily upload files from your photo library to a web server using a standard `multipart/form-data` POST request. It **does not** incorporate the tranfer of photo library images data from Objective-C to JavaScript (which is slow). The request are being made directly from Objective-C.
+# installation
+`npm install react-native-file-transfer`
+then just add the `RCTFileTransfer.m` file to your xcode project
+# how to use it
+When you properly add the `RCTFileTransfer.m` file to your xcode project you may now use it in the js files. Example usage:
+```javascript
+var { NativeMethods } = require('react-native');
+var obj = {
+    path, // either an 'assets-library' url (for files from photo library) or an image dataURL
+    uploadUrl,
+    fileName,
+    mimeType,
+    data: {
+        // whatever properties you wish to send in the request
+        // along with the uploaded file
+    }
+};
+NativeMethods.FileTransfer.upload(obj, res => {
+    // handle response
+    // it is an object with 'status' and 'data' properties
+    // if the file path protocol is not supported the status will be 0
+    // and the request won't be made at all
+});
+```
+**pull-requests welcome**
